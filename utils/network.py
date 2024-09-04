188297425077490cca444e42f3421bdb163a6871
moreorover <moreorover@null.computer>
2024-09-04 20:53:47 +0100

    scan network

--- utils/network.py
+++ utils/network.py
@@ -2,6 +2,8 @@ import netifaces
 import socket
 import uuid
 
+from scapy.all import ARP, Ether, srp
+
 def get_default_gateway():
     gateways = netifaces.gateways()
     default_gateway = gateways.get('default')
@@ -41,6 +43,31 @@ def get_mac_address_eth0(interface='eth0'):
     mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
     return mac_address
 
+def get_ip_range(default_gateway):
+    # Assuming a /24 subnet mask for simplicity
+    # We replace the last octet with '0/24' to define the range
+    ip_parts = default_gateway.split('.')
+    ip_parts[-1] = '0/24'
+    return '.'.join(ip_parts)
+
+def scan_network(ip_range):
+    # Create ARP request packet
+    arp = ARP(pdst=ip_range)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+
+    # Send packet and get the response
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    # Store the results in a list
+    devices = []
+
+    for sent, received in result:
+        # Append the IP and MAC address of each device to the list
+        devices.append({'IP': received.psrc, 'MAC': received.hwsrc})
+    
+    return devices
+
 if __name__ == "__main__":
     gateway_ip = get_default_gateway()
     print(f"Default Gateway IP Address: {gateway_ip}")
@@ -53,3 +80,14 @@ if __name__ == "__main__":
 
     mac_address_eth0 = get_mac_address()
     print(f"MAC Address eth0: {mac_address_eth0}")
+
+    ip_range = get_ip_range(gateway_ip)
+    print(ip_range)
+
+    network_devices = scan_network(ip_range)
+
+    print("Available devices in the network:")
+    print("IP" + " "*18+"MAC")
+
+    for device in network_devices:
+        print("{:16}    {}".format(device['IP'], device['MAC']))
